18ae0fd1938be8c6d3f3ef54478f555fd4a611ef
wyj <111860919+wyj-hb@users.noreply.github.com>
2024-07-15 18:03:23 +0800

    Create 按要求输出信息(2).cpp

---
+++ 顺序分支/按要求输出信息(2).cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout<<"********************"<<endl;
+    cout<<"Very Good!"<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<endl;
+    cout<<"********************"<<endl;
+}
